2ff0f2a46c57f21dea4fc6e418c80581fffc9a32
Onur Çankaya <onur.eren.cankaya@gmail.com>
2024-08-30 16:48:03 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-container max-width="600">
+    <h1 class="text-center">Todo App</h1>
+  </v-container>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from "vue";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "./style.css";
+import App from "./App.vue";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: "dark",
+  },
+});
+
+createApp(App).use(vuetify).mount("#app");
