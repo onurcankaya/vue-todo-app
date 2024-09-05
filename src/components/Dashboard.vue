8d1fc1eaca1a0e48f881d79f6460c71b23fd2b87
Onur Çankaya <onur.eren.cankaya@gmail.com>
2024-09-05 21:22:03 +0200

    implement logout

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,10 +1,28 @@
 <script setup lang="ts">
 import TodoList from "./TodoList.vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const logout = () => {
+  localStorage.removeItem("jwt");
+  router.push("/login");
+};
 </script>
 
 <template>
   <v-container max-width="600">
-    <h1 class="text-center">Todo App</h1>
+    <v-row>
+      <v-col>Todo App</v-col>
+      <v-col cols="auto">
+        <v-btn
+          text="Logout"
+          size="small"
+          variant="outlined"
+          @click="logout"
+        ></v-btn>
+      </v-col>
+    </v-row>
     <TodoList />
   </v-container>
 </template>
