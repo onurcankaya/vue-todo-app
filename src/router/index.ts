9c62937b95e1070d787743bc56c915a78aa0f4fb
Onur Çankaya <onur.eren.cankaya@gmail.com>
2024-09-15 12:10:56 +0200

    add navigation guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,20 +5,40 @@ import Dashboard from "../components/Dashboard.vue";
 
 const routes = [
   {
+    path: "/",
+    redirect: "/dashboard",
+  },
+  {
     path: "/register",
+    name: "Register",
     component: Register,
   },
   {
     path: "/login",
+    name: "Login",
     component: Login,
   },
   {
     path: "/dashboard",
+    name: "Dashboard",
     component: Dashboard,
+    meta: { requiresAuth: true },
   },
 ];
 
 export const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+// global navigation guard
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    const jwt = localStorage.getItem("jwt");
+
+    if (jwt) next();
+    else next({ name: "Login" });
+  } else {
+    next();
+  }
 });
